be6fd3c299b9fc20c4370e5e33b3ea48479b10c7
snakegtrr35 <ksyougo0918@gmail.com>
2020-07-25 14:46:08 +0900

    no message

--- Assets/Kanno/Script/Student.cs
+++ Assets/Kanno/Script/Student.cs
@@ -6,15 +6,48 @@ public class Student : MonoBehaviour
 {
     static private int Score = 0;
 
+    static private bool flag_ = true;
+
+    [Header("スコアが減るスパン")]
+    [SerializeField]
+    private float span = 3.0f;
+
+    private float time_ = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        time_ = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time_ += UnityEngine.Time.deltaTime;
+
+        if( ( span <= time_) && flag_)
+        {
+            time_ = 0.0f;
+            flag_ = false;
+
+            Score--;
+        }
+    }
+
+    /// <summary>
+    /// 最初に呼ばれるupdate
+    /// /// </summary>
+    private void FixedUpdate()
+    {
+        flag_ = true;
+    }
+
+    /// <summary>
+    /// 最後に呼ばれるupdate
+    /// </summary>
+    private void LateUpdate()
+    {
+        flag_ = false;
     }
 
     private void OnTriggerEnter(Collider other)
