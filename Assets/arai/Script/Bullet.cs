32126e86e6dee354163dfa14422052d4aa01f28e
arai <bythx790@gmail.com>
2020-07-25 14:01:11 +0900

    先生オブジェクトとFIrePointと弾の途中経過

---
+++ Assets/arai/Script/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [Header("弾速度")]
+    [SerializeField] float Speed_ = 1.0f;
+
+    /// <summary>
+    /// 速度代入用変数
+    /// </summary>
+    private Vector3 _speed = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _speed.x = Speed_;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += _speed;
+    }
+
+    
+}
---
+++ Assets/arai/Script/JyugyouWave.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JyugyouWave : Bullet
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Tagで判定
+        if (collision.transform.tag == "Syougaibutu")
+        {
+            Destroy(gameObject);
+        }
+
+        if(collision.transform.tag == "Student")
+        {
+            //なんかする
+        }
+    }
+}
