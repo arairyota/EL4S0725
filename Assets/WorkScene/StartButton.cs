805340f0ca069c4dafe6684be311feb2bd9c1ab0
xiaoyao1222 <b1968620@gmail.com>
2020-07-25 14:59:18 +0900

    タイトルシーン

---
+++ Assets/WorkScene/StartButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartButton : MonoBehaviour
+{
+    //ゲームシーン名
+    [SerializeField]
+    private string m_GameSceneName;
+
+    [SerializeField]
+    private Image Texture;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //遷移処理
+        if(Input.GetKeyDown(KeyCode.Return))
+            SceneManager.LoadScene(m_GameSceneName);
+
+        Color c = Texture.color;
+        c.a = Mathf.Sin(Time.time * 2);
+        Texture.color = c;
+    }
+}
